bdc7d5eb98323669cbe0e7dbd5a450163c96822b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-01 20:03:33 -0300

    Add version to html

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta version="@version('compact')">
 
         <title>{{ config('app.name') }} :: Front End :: {{ $currentPage }}</title>
 
