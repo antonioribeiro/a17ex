ab5a190a25ea7c37609342207a7b50164d65aae3
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-01 19:42:50 -0300

    Reenable cache

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,11 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-
-            /**
-             * DISABLED!!!!!!!!!!!! &&&&
-             */
-            //\Spatie\ResponseCache\Middlewares\CacheResponse::class,
+            \Spatie\ResponseCache\Middlewares\CacheResponse::class,
         ],
 
         'api' => ['throttle:60,1', 'bindings'],
