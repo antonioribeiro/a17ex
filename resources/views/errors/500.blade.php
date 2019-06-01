155f893e4f0791e48cb5dee6d3d7218289001c30
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-01 17:32:57 -0300

    Add 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+<div style="text-align: center; font-family: Arial;">
+    <div style="font-size: 25em">500</div>
+    <p>Yeah, this is our fault, sorry, but we are already fixing it.</p>
+    <p><a href="/">go home</a></p>
+</div>
