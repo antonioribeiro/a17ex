71fea0f5130e91e303ebcbcfe2bce77d7b036877
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-02 14:47:53 -0300

    Deploy to Github

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,8 @@
-if [ ! -f artisan ]
-then
-    echo "The artisan command is missing"
-    exit
-fi
-
-#git reset --hard HEAD
-#git pull origin production
-#composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
-#echo "" | sudo -S service php7.3-fpm reload
-
-php artisan cache:clear
-php artisan config:cache
-php artisan view:clear
-php artisan route:cache
-#php artisan varnish:flush
-php artisan migrate --force
+npm run dev
+git add public/*
+git commit -m "Compile to production"
+php artisan version:absorb
+git add config/version.yml
+git commit -m "Update version"
+git push origin master
+git push origin master:staging
