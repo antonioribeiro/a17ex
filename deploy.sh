adec14f5c4a6e5dbe88cec28f793d8cd70260db8
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-02 20:42:30 -0300

    Add some cache clearing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 npm run dev
 git add public/*
 git commit -m "Compile to production"
+php artisan cache:clear
+php artisan config:clear
 php artisan version:absorb
 git add config/version.yml
 git commit -m "Update version"
