51bdacbe6459f8b97c2de7aab03cef07e75312ca
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-02 19:18:08 -0300

    Show author info

--- resources/views/site/news/partials/show.blade.php
+++ resources/views/site/news/partials/show.blade.php
@@ -4,7 +4,22 @@
     @endif
 
     <h1>{{ $article->title }}</h1>
-    <br><br><br>
+
+    <a href="{{ $article->author->showUrl }}">
+        <div class="row" style="margin-top: 5px;">
+            <div class="col-3">
+                <img src="{{ $article->author->image('avatar') }}" alt="" width="35" style="padding-right: 8px">
+            </div>
+            <div class="col-3">
+                <ul>
+                    <li style="font-size: 0.8em !important;">{{ $article->author->name }}</li>
+                    <li style="font-size: 0.8em !important;">{{ $article->author->occupation->title }}</li>
+                </ul>
+            </div>
+        </div>
+    </a>
+
+    <br><br>
 </div>
 
 <div class='content'>
