70c03cbd20f4f68362e22657a3a40f95cf089e8d
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-03 19:26:37 -0300

    Add hostname to meta

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta version="@version('compact')">
+        <meta data-version="@version('compact')">
+        <meta data-hostname="{{ gethostname() }}">
 
         <title>{{ config('app.name') }} :: Front End :: {{ $currentPage }}</title>
 
